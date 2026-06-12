4b0d46d45be71e50d8f72ac6eed75cc453ac3c60
Артём <artem@MacBook-Pro-Artem.local>
2026-06-12 11:32:01 +0300

    Add GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/VueSneakers/',   
   cacheDir: resolve(__dirname, '.vite_cache'),
   css: {
     preprocessorOptions: {
